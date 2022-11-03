96f5a71c9f5fbefb6fbf28b0893db122be8f0b7a
ngud-119 <passiondev7@gmail.com>
2022-11-03 19:05:57 +0100

    Added some default values

--- src/Services/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog.API/Entities/Product.cs
@@ -7,19 +7,19 @@ public class Product
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string Id { get; set; } = "";
 
     // To set the column name inside the db:
     [BsonElement("Name")]
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
 
-    public string Category { get; set; }
+    public string Category { get; set; } = "";
 
-    public string Summary { get; set; }
+    public string Summary { get; set; } = "";
 
-    public string Description { get; set; }
+    public string Description { get; set; } = "";
 
-    public string ImageFile { get; set; }
+    public string ImageFile { get; set; } = "";
 
     public decimal Price { get; set; }
 }
