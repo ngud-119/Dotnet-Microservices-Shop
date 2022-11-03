7fcedf1c0c1a6590b83875890182bef3c29f2e86
ngud-119 <passiondev7@gmail.com>
2022-11-03 21:45:20 +0100

    Added the Redis caching package to the basket api

--- src/Services/Basket.API/Program.cs
+++ src/Services/Basket.API/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisCache(opt =>
+{
+    opt.Configuration = builder.Configuration["CacheSettings:ConnectionString"];
+});
 
 var app = builder.Build();
 
