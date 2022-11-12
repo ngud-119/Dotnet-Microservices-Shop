da5649733a1ee2b5394271ff8388ce63eba19afa
ngud-119 <passiondev7@gmail.com>
2022-11-12 22:04:20 +0100

    Removed default grpc services

--- src/Services/Discount.GRPC/Program.cs
+++ src/Services/Discount.GRPC/Program.cs
@@ -1,6 +1,5 @@
 using Discount.GRPC.Extensions;
 using Discount.GRPC.Repositories;
-using Discount.GRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +15,7 @@ var app = builder.Build();
 app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
