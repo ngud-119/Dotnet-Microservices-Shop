c7b7f0578dbbe2427da5ad85a1ff1813d0172e56
ngud-119 <passiondev7@gmail.com>
2022-12-06 11:40:04 +0100

    Created some custom exception

---
+++ src/Services/Ordering.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+namespace Ordering.Application.Exceptions;
+
+public class NotFoundException : ApplicationException
+{
+    public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) was not found.")
+    {
+    }
+}
