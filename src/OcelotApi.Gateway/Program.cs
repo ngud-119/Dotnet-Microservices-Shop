6068925a4c6a471ed9b0c8f5b40c17574b1b10fe
ngud-119 <passiondev7@gmail.com>
2023-02-01 21:22:23 +0100

    Added local configuration for ocelot

--- src/OcelotApi.Gateway/Program.cs
+++ src/OcelotApi.Gateway/Program.cs
@@ -7,6 +7,11 @@ builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
+builder.Host.ConfigureAppConfiguration(opt =>
+{
+    opt.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json");
+});
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
