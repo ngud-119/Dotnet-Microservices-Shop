3ed83ae9e32cf751cb2e1d48f96767581595d78a
ngud-119 <passiondev7@gmail.com>
2023-02-01 20:32:33 +0100

    Added ocelot api

--- src/OcelotApi.Gateway/Program.cs
+++ src/OcelotApi.Gateway/Program.cs
@@ -1,11 +1,16 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+app.UseOcelot();
 app.Run();
