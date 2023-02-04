2126d5f842c4bd72da1f49e4dbd0bc0db705c201
ngud-119 <passiondev7@gmail.com>
2023-02-04 21:22:34 +0100

    Finishing creating DTO classes in shopping aggregator

---
+++ src/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -0,0 +1,8 @@
+namespace Shopping.Aggregator.Models;
+
+public class ShoppingModel
+{
+    public string UserName { get; set; }
+    public BasketModel BasketWithProducts { get; set; }
+    public IEnumerable<OrderResponseModel> Orders { get; set; }
+}
