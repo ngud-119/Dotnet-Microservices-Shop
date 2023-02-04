a6e6d406f45d4fb41e6fe6f7b8ac17b9d21b47fb
ngud-119 <passiondev7@gmail.com>
2023-02-04 23:04:06 +0100

    Implemented Order service

--- src/Shopping.Aggregator/Services/OrderService.cs
+++ src/Shopping.Aggregator/Services/OrderService.cs
@@ -1,10 +1,19 @@
+using Shopping.Aggregator.Extensions;
 using Shopping.Aggregator.Models;
 using Shopping.Aggregator.Services;
 
 public class OrderService : IOrderService
 {
-    public Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+    private readonly HttpClient httpClient;
+
+    public OrderService(HttpClient httpClient)
     {
-        throw new NotImplementedException();
+        this.httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+    {
+        var response = await httpClient.GetAsync($"/api/v1/Order/{userName}");
+        return await response.ReadContentAs<List<OrderResponseModel>>();
     }
 }
