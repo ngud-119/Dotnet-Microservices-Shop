ee545e39fe7e1cb128677f1c77b1849506213609
ngud-119 <passiondev7@gmail.com>
2023-02-04 23:00:01 +0100

    Implemented basket service

--- src/Shopping.Aggregator/Services/BasketService.cs
+++ src/Shopping.Aggregator/Services/BasketService.cs
@@ -1,11 +1,20 @@
+using Shopping.Aggregator.Extensions;
 using Shopping.Aggregator.Models;
 
 namespace Shopping.Aggregator.Services;
 
 public class BasketService : IBasketService
 {
-    public Task<BasketModel> GetBasket(string username)
+    private readonly HttpClient httpClient;
+
+    public BasketService(HttpClient httpClient)
     {
-        throw new NotImplementedException();
+        this.httpClient = httpClient;   
+    }
+
+    public async Task<BasketModel> GetBasket(string username)
+    {
+        var response = await httpClient.GetAsync($"/api/v1/Basket/{username}");
+        return await response.ReadContentAs<BasketModel>();
     }
 }
