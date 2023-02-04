f34f636245e792f231437c13e7a019358c546b55
ngud-119 <passiondev7@gmail.com>
2023-02-04 21:16:05 +0100

    Create catalog model in shopping aggregator

---
+++ src/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,10 @@
+namespace Shopping.Aggregator.Models;
+
+public class BasketModel
+{
+    public string UserName { get; set; }
+    public List<BasketItemExtendedModel> Items { get; set; } = new List<BasketItemExtendedModel>();
+
+    public decimal TotalPrice { get; set; }
+    
+}
